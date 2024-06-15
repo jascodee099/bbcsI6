20579038f74e1a463dd83f37c2926980e67b56b9
Jas <150653828+jascodee099@users.noreply.github.com>
2024-06-15 12:54:18 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+# main_page
+import streamlit as st
+
+st.set_page_config(page_title = 'Learning Hub!',page_icon = ':school:')
+st.sidebar.markdown(" Main Page🎈")
+st.title('Hey There! Welcome to Learning Hub! Log in now and ask your question!')
+
+#python -m streamlit run main.py
